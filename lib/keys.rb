912a7cfa136424e0b5fd45c9d19b1095a921321d
Brad Schrag <brad.schrag@gmail.com>
2018-11-09 04:14:41 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -10,9 +10,5 @@ require 'pry'
 
 def get_the_keys(groceries)
   #code your solution here!
-  groceries.each do |category, list|
-    list.collect do |item|
-      item
-    end
-  end
+  groceries.keys
 end
